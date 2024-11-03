a80bc7c02bd1919cc377f1fd5e373346ff03e18b
annikola <annikolaprof@gmail.com>
2024-11-03 06:28:32 +0200

    Changed between centroid splitting to hyperplain splitting

--- hw1.c
+++ hw1.c
@@ -8,26 +8,35 @@
 
 #define MAX_THREADS 5
 #define MAX_CLUSTERS 2
+#define MAX_SET_SPLIT 2
 
-void write_2D_array_to_matfile(const char *filename);
+void write_2D_array_to_matfile(const char *filename, const char *array_name, double **_2D_array, int c_size, int d);
+void *hyper_binary_split(hyper_set hyper_subset, int depth);
 double random_double(double min, double max);
-void *distance_calculator(void *ccN);
+void *distance_calculator(void *args);
 
-typedef struct knn_centroid {
-    int center_index;
+typedef struct {
+    double *q_curr;
+    int ccp_length;
+    int d;
+} ThreadArgs;
+
+typedef struct {
     double **neighbors;
-} knn_centroid;
+    int set_size;
+    int d;
+    double *coeffs;
+    double b_value;
+} hyper_set;
 
 int main(int argc, char *argv[]) {
 
-    int i, j, k, l, t;
-    int c_size, q_size, d, closest_centroid;
+    int i, j, t;
+    int c_size, q_size, d;
     double **c, **q;
-    double **temp[MAX_CLUSTERS];
-    double diff_squares_sum, min_euclid_distance;
-    double curr_centroid_center_distances[MAX_CLUSTERS];
-    knn_centroid clusters[MAX_CLUSTERS];
     pthread_t thread_ids[MAX_THREADS];
+    clock_t start_t;
+    ThreadArgs *args;
 
     /* THA FYGEI STO TELOS!!! --> */
     
@@ -58,51 +67,16 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    write_2D_array_to_matfile("test.mat");
-
     for (i = 0; i < q_size; i++) {
         for (j = 0; j < d; j++) {
             q[i][j] = random_double(1.0, 100.0);
         }
     }
 
+    write_2D_array_to_matfile("test.mat", "C", c, c_size, d);
+    write_2D_array_to_matfile("test2.mat", "Q", q, q_size, d);
+
     /* <-- THA FYGEI STO TELOS!!! */
-
-    for (l = 0;  l < MAX_CLUSTERS; l++) {
-        clusters[l].neighbors = (double **)malloc(c_size * sizeof(double *));
-    }
-
-    for (i = 0; i < MAX_CLUSTERS; i++) {
-        clusters[i].center_index = rand() % c_size;
-    }
-
-    for (k = 0; k < MAX_CLUSTERS; k++) {
-        temp[k] = clusters[k].neighbors;
-    }
-    for (i = 0; i < c_size; i++) {
-        for (k = 0; k < MAX_CLUSTERS; k++) {
-            diff_squares_sum = 0.0;
-            for (j = 0; j < d; j++) {
-                diff_squares_sum += pow(c[i][j] - c[clusters[k].center_index][j], 2);
-            }
-            curr_centroid_center_distances[k] = sqrt(diff_squares_sum);
-        }
-
-        min_euclid_distance = 1e10; // MAX_DOUBLE...change that!
-        for (k = 0; k < MAX_CLUSTERS; k++) {
-            if (curr_centroid_center_distances[k] < min_euclid_distance) {
-                min_euclid_distance = curr_centroid_center_distances[k];
-                closest_centroid = k;
-            }
-        }
-
-        *(temp[closest_centroid]) = c[i];
-        temp[closest_centroid]++;
-    }
-
-    for (k = 0; k < MAX_CLUSTERS; k++) {
-        clusters[k].neighbors = realloc(clusters[k].neighbors, (temp[k] - clusters[k].neighbors) * sizeof(double *));
-    }
 
     // for (k = 0; k < MAX_CLUSTERS; k++) {
     //     for (i = 0; i < temp[k] - clusters[k].neighbors; i++) {
@@ -114,39 +88,140 @@ int main(int argc, char *argv[]) {
     //     printf("\n");
     // }
 
+    // start_t = clock();
+    // args = (ThreadArgs *)malloc(sizeof(ThreadArgs)); ???
+    // args = malloc(sizeof(ThreadArgs));
     // for (t = 0; t < MAX_THREADS; t++) {
-    //     pthread_create(&thread_ids[t], NULL, distance_calculator, &cc2);
+    //     args->q_curr = q[0];
+    //     args->ccp_length = 0 ???
+    //     args->d = d;
+    //     pthread_create(&thread_ids[t], NULL, distance_calculator, args);
     // }
 
     // for (t = 0; t < MAX_THREADS; t++) {
     //     pthread_join(thread_ids[t], NULL);
     // }
 
+    // printf("Multithreaded application finished in: %lf seconds!\n", (double)(clock() - start_t) / CLOCKS_PER_SEC);
+
+    free(c);
+    free(q);
+
     return 0;
 }
 
-void write_2D_array_to_matfile(const char *filename) {
+void *hyper_binary_split(hyper_set hyper_subset, int depth) {
 
-    MATFile *matFile = matOpen(filename, "w");
+    int i, j;
+    double *random_point_1, *random_point_2;
+    double midpoint[hyper_subset.d], normal_vector[hyper_subset.d];
+    double beta, hyper_position;
+    hyper_set new_hyper_subset_1, new_hyper_subset_2;
+
+    random_point_1 = hyper_subset.neighbors[rand() % hyper_subset.set_size];
+    random_point_2 = hyper_subset.neighbors[rand() % hyper_subset.set_size];
+
+    for (j = 0; j < hyper_subset.d; j++) {
+        midpoint[j] = (random_point_1[j] + random_point_2[j]) / 2.0;
+    }
+
+    for (j = 0; j < hyper_subset.d; j++) {
+        normal_vector[j] = random_point_2[j] - random_point_1[j];
+    }
+
+    beta = 0.0;
+    for (j = 0; j < hyper_subset.d; j++) {
+        beta += normal_vector[j] * midpoint[j];
+    }
+
+    hyper_subset.coeffs = normal_vector;
+    hyper_subset.b_value = beta;
+
+    new_hyper_subset_1.neighbors = (double **)malloc(hyper_subset.set_size * sizeof(double *));
+    new_hyper_subset_2.neighbors = (double **)malloc(hyper_subset.set_size * sizeof(double *));
+
+    new_hyper_subset_1.d = new_hyper_subset_2.d = hyper_subset.d;
+    new_hyper_subset_1.set_size = new_hyper_subset_2.set_size = 0;
+    for (i = 0; i < hyper_subset.set_size; i++) {
+        hyper_position = 0.0;
+        for (j = 0; j < hyper_subset.d; j++) {
+            hyper_position += normal_vector[j] * hyper_subset.neighbors[i][j];
+        }
+
+        if (hyper_position > beta) {
+            new_hyper_subset_1.neighbors[new_hyper_subset_1.set_size] = hyper_subset.neighbors[i];
+            new_hyper_subset_1.set_size++;
+        }
+        else {
+            new_hyper_subset_2.neighbors[new_hyper_subset_2.set_size] = hyper_subset.neighbors[i];
+            new_hyper_subset_2.set_size++;
+        }
+    }
+
+    new_hyper_subset_1.neighbors = realloc(new_hyper_subset_1.neighbors, new_hyper_subset_1.set_size * sizeof(double *));
+    new_hyper_subset_2.neighbors = realloc(new_hyper_subset_2.neighbors, new_hyper_subset_2.set_size * sizeof(double *));
+
+    for (i = 0; i < new_hyper_subset_1.set_size; i++) {
+        for (j = 0; j < new_hyper_subset_1.d; j++) {
+            printf("%lf ", new_hyper_subset_1.neighbors[i][j]);
+        }
+        printf("\b\n");
+    }
+    printf("\n");
+
+    for (i = 0; i < new_hyper_subset_2.set_size; i++) {
+        for (j = 0; j < new_hyper_subset_2.d; j++) {
+            printf("%lf ", new_hyper_subset_2.neighbors[i][j]);
+        }
+        printf("\b\n");
+    }
+    printf("\n\n\n\n");
+
+    if (new_hyper_subset_1.set_size > depth) {
+        hyper_binary_split(new_hyper_subset_1, depth);
+    }
+    
+    if (new_hyper_subset_2.set_size > depth) {
+        hyper_binary_split(new_hyper_subset_2, depth);
+    }
+
+    return;
+}
+
+void write_2D_array_to_matfile(const char *filename, const char *array_name, double **_2D_array, int c_size, int d) {
+
+    int i, j;
+    double *matData;
+    MATFile *matFile;
+    mxArray *matArray;
+
+    matFile = matOpen(filename, "w");
     if (matFile == NULL) {
         printf("Error opening MAT-file %s\n", filename);
         return;
     }
 
-    double array[3][3] = { {1.0, 2.0, 3.0},
-                           {4.0, 5.0, 6.0},
-                           {7.0, 8.0, 9.0} };
-
-    mxArray *matArray = mxCreateDoubleMatrix(3, 3, mxREAL);
+    matArray = mxCreateDoubleMatrix(c_size, d, mxREAL);
     if (matArray == NULL) {
         printf("Could not create mxArray.\n");
         matClose(matFile);
         return;
     }
 
-    memcpy(mxGetPr(matArray), array, sizeof(array));
+    // matData = mxGetPr(matArray);
+    // for (i = 0; i < c_size; i++) {
+    //     memcpy(matData + i * d, _2D_array[i], d * sizeof(double));
+    // }
 
-    matPutVariable(matFile, "myArray", matArray);
+    // MATLAB saves in column major order, therefore we take the transpose...
+    matData = mxGetPr(matArray);
+    for (i = 0; i < c_size; i++) {
+        for (j = 0; j < d; j++) {
+            matData[j * c_size + i] = _2D_array[i][j];
+        }
+    }
+
+    matPutVariable(matFile, array_name, matArray);
 
     mxDestroyArray(matArray);
     matClose(matFile);
@@ -162,6 +237,21 @@ double random_double(double min, double max) {
     return min + scale * (max - min);
 }
 
-void *distance_calculator(void *ccN) {
-    printf("%d\n", *(int *)ccN);
+void *distance_calculator(void *args) {
+
+    int i, j;
+    double diff_squares_sum;
+    double closest_cluster_part_distances[((ThreadArgs *)args)->ccp_length];
+    ThreadArgs *cargs;
+
+    cargs = (ThreadArgs *)args;
+    for (i = 0; i < ((ThreadArgs *)cargs)->ccp_length; i++) {
+        diff_squares_sum = 0.0;
+        for (j = 0; j < ((ThreadArgs *)cargs)->d; j++) {
+            diff_squares_sum += pow(((ThreadArgs *)cargs)->q_curr[j] - ((ThreadArgs *)cargs)->closest_cluster_part.neighbors[i][j], 2);
+        }
+        closest_cluster_part_distances[i] = sqrt(diff_squares_sum);
+    }
+
+    printf("Thread finished!\n");
 }
